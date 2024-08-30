be84fe6bda90f2e8006461c55a0713afe388b152
Kai Aragaki <kaiaragaki@proton.me>
2024-08-30 15:39:20 -0400

    make pcr_plate_view more generic

--- R/pcr_plate_view.R
+++ R/pcr_plate_view.R
@@ -11,10 +11,19 @@
 #'
 #' @examples
 #' system.file("extdata", "untidy-pcr-example.xls", package = "amplify") |>
-#'   pcr_tidy() |>
+#'   read_pcr() |>
 #'   pcr_plate_view()
-
 pcr_plate_view <- function(pcr, fill = target_name) {
+  UseMethod("pcr_plate_view")
+}
+
+#' @export
+pcr_plate_view.pcr <- function(pcr, fill = target_name) {
   pcr <- tidy_if_not(pcr)
   gplate::gp_plot(pcr$data, {{fill}})
+}
+
+#' @export
+pcr_plate_view.data.frame <- function(pcr, fill = target_name) {
+  gplate::gp_plot(pcr, {{fill}})
 }
